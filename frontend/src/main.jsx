4f51c28fda1bf303c88458722f2696a928f77271
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-13 15:08:17 -0400

    refactor: Implement react-router for routing in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+// router
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index path='/' element={<App />}></Route>)
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
 )
