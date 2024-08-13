87ef4fe058529ff0184e7365824c367a0817b784
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-13 19:31:47 -0400

    refactor: Update Landing page to include logo, navigation, and styled wrapper

--- frontend/src/pages/Home/Landing.jsx
+++ frontend/src/pages/Home/Landing.jsx
@@ -12,25 +12,27 @@ const Landing = () => {
         <img src={logo} alt='jobber logo' className='logo' />
       </nav>
       <div className='container page'>
-        <h1>
-          Job <span>Tracking</span> App
-        </h1>
-        <p>
-          I'm baby beard taiyaki humblebrag tumeric pitchfork DSA lyft
-          sustainable pabst master cleanse man braid gluten-free. Jawn vinyl
-          single-origin coffee four loko. Bruh hexagon retro listicle chillwave
-          bushwick deep v four dollar toast, 8-bit grailed yr PBR&B. JOMO
-          whatever helvetica, church-key quinoa mumblecore DIY intelligentsia
-          craft beer keytar.
-        </p>
-        <Link to='/register' className='btn register-link'>
-          Register
-        </Link>
-        <Link to='/login' className='btn'>
-          Login / Demo User
-        </Link>
+        <div className='info'>
+          <h1>
+            Job <span>Tracking</span> App
+          </h1>
+          <p>
+            I&apos;m baby beard taiyaki humblebrag tumeric pitchfork DSA lyft
+            sustainable pabst master cleanse man braid gluten-free. Jawn vinyl
+            single-origin coffee four loko. Bruh hexagon retro listicle
+            chillwave bushwick deep v four dollar toast, 8-bit grailed yr PBR&B.
+            JOMO whatever helvetica, church-key quinoa mumblecore DIY
+            intelligentsia craft beer keytar.
+          </p>
+          <Link to='/register' className='btn register-link'>
+            Register
+          </Link>
+          <Link to='/login' className='btn'>
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
       </div>
-      <img src={main} alt='job hunt' className='img main-img' />
     </Wrapper>
   )
 }
