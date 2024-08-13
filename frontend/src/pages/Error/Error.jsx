72d37a0ff38ab07c2a8acada0c4057ae6693d508
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-13 18:35:26 -0400

    refactor: Update Error component to display a 404 Not Found message with a link to go back to the dashboard

---
+++ frontend/src/pages/Error/Error.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+const Error = () => {
+  return (
+    <div>
+      404 Not Found <Link to='/'>Go Back to Dashboard</Link>
+    </div>
+  )
+}
+
+export default Error
