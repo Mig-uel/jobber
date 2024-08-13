eedee1b4b88134464a1fbac4e92c1c76728f4df2
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-13 17:54:46 -0400

    refactor: Update Register component to include login link

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,12 @@
-const Resgister = () => {
-  return <></>
+import { Link } from 'react-router-dom'
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+      <Link to='/login'>Login</Link>
+    </div>
+  )
 }
 
-export default Resgister
+export default Register
