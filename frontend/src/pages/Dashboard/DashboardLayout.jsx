af8d9df4911cc07a091234c18c4eaeb4a92e2952
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-13 18:24:30 -0400

    refactor: Update DashboardLayout to include heading and Outlet

--- frontend/src/pages/Dashboard/DashboardLayout.jsx
+++ frontend/src/pages/Dashboard/DashboardLayout.jsx
@@ -1,5 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
 const DashboardLayout = () => {
-  return <></>
+  return (
+    <>
+      <h1>Dashboard Layout</h1>
+      <Outlet />
+    </>
+  )
 }
 
 export default DashboardLayout
