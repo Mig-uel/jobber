2ddb3ee8f3cbb4da9267c08cca7263c0e6b9fb11
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-13 19:52:10 -0400

    refactor: Improve Error component to handle 404 errors and display a custom message

--- frontend/src/pages/Error/Error.jsx
+++ frontend/src/pages/Error/Error.jsx
@@ -1,13 +1,32 @@
 import { Link, useRouteError } from 'react-router-dom'
+import { Wrapper } from '../../styled/Error'
+
+// images
+import img from '../../assets/images/not-found.svg'
 
 const Error = () => {
   const error = useRouteError()
   console.log(error)
 
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt='not found' />
+          <h3>Oops...</h3>
+          <p>we can&apos;t seem to find the page you are looking for</p>
+          <Link to='/dashboard'>Back Home</Link>
+        </div>
+      </Wrapper>
+    )
+  }
+
   return (
-    <div>
-      {error.status} {error.statusText} <Link to='/'>Go Back to Dashboard</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>Something went wrong...</h3>
+      </div>
+    </Wrapper>
   )
 }
 
