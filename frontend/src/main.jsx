df885ba12abb27659a9e36e3ad0bbc49ee66b90b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-13 17:54:04 -0400

    refactor: Update main.jsx to implement react-router for routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,11 +9,31 @@ import {
   RouterProvider,
 } from 'react-router-dom'
 
+// components
 import App from './App.jsx'
+import {
+  DashboardLayout,
+  AddJob,
+  Admin,
+  DeleteJob,
+  EditJob,
+  Error,
+  Landing,
+  Login,
+  Profile,
+  Register,
+  Stats,
+} from './pages'
 import './index.css'
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index path='/' element={<App />}></Route>)
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/dashboard' element={<DashboardLayout />}></Route>
+    </Route>
+  )
 )
 
 createRoot(document.getElementById('root')).render(
