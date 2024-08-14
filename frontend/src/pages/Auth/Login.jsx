2c9dbd6c98d46710c35158a53912b28960e0458e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-13 20:40:58 -0400

    refactor: Update Login page with styled components and form for user login

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,11 +1,34 @@
 import { Link } from 'react-router-dom'
 
+import { Wrapper } from '../../styled/RegisterAndLogin'
+import { Logo, FormRow } from '../../components'
+
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to='/register'>Register</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Login</h4>
+
+        <FormRow type='email' name='email' defaultValue='john@email.com' />
+        <FormRow type='password' name='password' defaultValue='123456' />
+
+        <button type='submit' className='btn btn-block'>
+          Login
+        </button>
+
+        <button type='button' className='btn btn-block'>
+          Explore the App
+        </button>
+
+        <p>
+          Not a member yet?
+          <Link to='/register' className='member-btn'>
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   )
 }
 
