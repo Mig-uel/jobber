e486cabcbbad65d693c3941322b610adab401444
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-13 20:34:25 -0400

    refactor: Add FormRow component to Register page for form input fields

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 
 import { Wrapper } from '../../styled/RegisterAndLogin'
-import { Logo } from '../../components'
+import { Logo, FormRow } from '../../components'
 
 const Register = () => {
   return (
@@ -9,22 +9,27 @@ const Register = () => {
       <form className='form'>
         <Logo />
         <h4>Register</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            Name
-          </label>
-          <input
-            type='text'
-            id='name'
-            name='name'
-            className='form-input'
-            defaultValue='John'
-            required
-          />
-        </div>
+
+        <FormRow
+          type='text'
+          name='firstName'
+          defaultValue='John'
+          labelText='First Name'
+        />
+        <FormRow
+          type='text'
+          name='lastName'
+          defaultValue='Smith'
+          labelText='Last Name'
+        />
+        <FormRow type='text' name='location' defaultValue='Brooklyn' />
+        <FormRow type='email' name='email' defaultValue='john@email.com' />
+        <FormRow type='password' name='password' defaultValue='123456' />
+
         <button type='submit' className='btn btn-block'>
           Register
         </button>
+
         <p>
           Already a member?
           <Link to='/login' className='member-btn'>
