7953c474268c52ae993a1c36c12f89d1041d24be
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-14 18:59:38 -0400

    feat: Add toggle functionality for dark theme and sidebar in DashboardLayout.jsx

--- frontend/src/pages/Dashboard/DashboardLayout.jsx
+++ frontend/src/pages/Dashboard/DashboardLayout.jsx
@@ -1,8 +1,20 @@
+import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Wrapper } from '../../styled/Dashboard'
 import { DesktopSidebar, MobileSidebar, Navbar } from '../../components'
 
 const DashboardLayout = () => {
+  const user = { name: 'John' }
+  const [toggleSidebar, setToggleSidebar] = useState(false)
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+  // handlers
+  const toggleDarkThemeHandler = () => {
+    console.log('toggle dark theme')
+  }
+
+  const toggleSidebarHandler = () => setToggleSidebar((prev) => !prev)
+
   return (
     <Wrapper>
       <main className='dashboard'>
