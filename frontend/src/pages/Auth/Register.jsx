0b6cf46b01e18eec0f1f9a53e12d4b3bc85a385a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-13 20:04:57 -0400

    refactor: Update Register page to use styled components and add form for user registration

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,11 +1,38 @@
 import { Link } from 'react-router-dom'
 
+import { Wrapper } from '../../styled/RegisterAndLogin'
+import { Logo } from '../../components'
+
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to='/login'>Login</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Register</h4>
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            Name
+          </label>
+          <input
+            type='text'
+            id='name'
+            name='name'
+            className='form-input'
+            defaultValue='John'
+            required
+          />
+        </div>
+        <button type='submit' className='btn btn-block'>
+          Register
+        </button>
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   )
 }
 
