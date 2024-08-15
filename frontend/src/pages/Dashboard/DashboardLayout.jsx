cf57a9af3530f638a93302d5fe8c28a581500f04
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-14 20:04:59 -0400

    refactor: Update DashboardLayout component with new structure and components

--- frontend/src/pages/Dashboard/DashboardLayout.jsx
+++ frontend/src/pages/Dashboard/DashboardLayout.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
+import { createContext, useContext, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Wrapper } from '../../styled/Dashboard'
 import { DesktopSidebar, MobileSidebar, Navbar } from '../../components'
 
+// context
+const DashboardContext = createContext()
+
 const DashboardLayout = () => {
   const user = { name: 'John' }
-  const [toggleSidebar, setToggleSidebar] = useState(false)
+  const [showSidebar, setShowSidebar] = useState(false)
   const [isDarkTheme, setIsDarkTheme] = useState(false)
 
   // handlers
-  const toggleDarkThemeHandler = () => {
-    console.log('toggle dark theme')
+  const toggleDarkTheme = () => console.log('toggle dark theme')
+  const toggleSidebar = () => setShowSidebar((prev) => !prev)
+  const logoutUser = () => console.log('logged out')
+
+  // context value
+  const value = {
+    user,
+    showSidebar,
+    isDarkTheme,
+    toggleDarkTheme,
+    toggleSidebar,
+    logoutUser,
   }
 
-  const toggleSidebarHandler = () => setToggleSidebar((prev) => !prev)
-
   return (
-    <Wrapper>
-      <main className='dashboard'>
-        <MobileSidebar />
-        <DesktopSidebar />
-        <div>
-          <Navbar />
-          <div className='dashboard-page'>
-            <Outlet />
+    <DashboardContext.Provider value={value}>
+      <Wrapper>
+        <main className='dashboard'>
+          <MobileSidebar />
+          <DesktopSidebar />
+          <div>
+            <Navbar />
+            <div className='dashboard-page'>
+              <Outlet />
+            </div>
           </div>
-        </div>
-      </main>
-    </Wrapper>
+        </main>
+      </Wrapper>
+    </DashboardContext.Provider>
   )
 }
+
+// custom context hook
+export const useDashboardContext = () => useContext(DashboardContext)
 
 export default DashboardLayout
