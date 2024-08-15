d50fb231c39813aed9f96ea780e28f3c43409737
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-15 16:32:21 -0400

    refactor: Add comments explaining the 404 and error middleware

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ app.use('/api/v1/jobs', jobsRouter)
 // not found route
 app.use('*', (req, res) => {
   res.status(404).json({ message: 'Not found' })
-})
+}) // triggered if the resource does not exists
 
 // custom error handling
-app.use(errorHandler)
+app.use(errorHandler) // triggered by existing controllers (valid routes)
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`SERVER RUNNING ON PORT: ${port}`)
