52e7354a294393e3f527f9264cc14dd9a54d4352
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-14 20:28:24 -0400

    refactor: Update Navbar component with toggle button and logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import { Wrapper } from '../styled/Navbar'
+import { FaAlignLeft } from 'react-icons/fa'
+import Logo from './Logo'
+import { useDashboardContext } from '../pages/Dashboard/DashboardLayout'
 
 const Navbar = () => {
-  return <Wrapper></Wrapper>
+  const { toggleSidebar } = useDashboardContext()
+
+  return (
+    <Wrapper>
+      <div className='nav-center'>
+        <button type='button' className='toggle-btn' onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className='logo-text'>Dashboard</h4>
+        </div>
+        <div className='btn-container'>toggle/logout</div>
+      </div>
+    </Wrapper>
+  )
 }
 
 export default Navbar
