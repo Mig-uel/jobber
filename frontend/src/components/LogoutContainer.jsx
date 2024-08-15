1f69c94babb152ca670b6b8b2898215c9e99619a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-14 22:03:51 -0400

    feat: Add LogoutContainer component for user logout functionality

---
+++ frontend/src/components/LogoutContainer.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { useDashboardContext } from '../pages/Dashboard/DashboardLayout'
+import { Wrapper } from '../styled/LogoutContainer'
+import { FaUserCircle, FaCaretDown } from 'react-icons/fa'
+
+const LogoutContainer = () => {
+  const [showLogout, setShowLogout] = useState(false)
+  const { user, logoutUser } = useDashboardContext()
+
+  return (
+    <Wrapper>
+      <button
+        type='button'
+        className='btn logout-btn'
+        onClick={() => setShowLogout((prev) => !prev)}
+      >
+        <FaUserCircle /> {user?.name} <FaCaretDown />
+      </button>
+
+      <div className={`dropdown ${showLogout && 'show-dropdown'}`}>
+        <button type='button' className='dropdown-btn' onClick={logoutUser}>
+          logout
+        </button>
+      </div>
+    </Wrapper>
+  )
+}
+
+export default LogoutContainer
