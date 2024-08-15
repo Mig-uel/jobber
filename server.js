d14a89cd91ffada791c309c9f0868a68eb4840b4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-15 14:52:32 -0400

    refactor: Update server.js with Express server configuration and add morgan middleware for logging

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import morgan from 'morgan'
 import { configDotenv } from 'dotenv'
 configDotenv({})
 
 const port = process.env.PORT
 const app = express()
 
+app.use(morgan('combined'))
 app.use(express.json())
 
 app.listen(port, '0.0.0.0', () => {
