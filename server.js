036933ee1aefd10b1f4502cb7bed637062736152
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-15 14:57:45 -0400

    refactor: Update server.js with Express server configuration and add conditional morgan middleware for logging

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ import morgan from 'morgan'
 import { configDotenv } from 'dotenv'
 configDotenv({})
 
-const port = process.env.PORT
+const port = process.env.PORT || 5100
 const app = express()
 
-app.use(morgan('combined'))
+// middleware
+process.env.NODE_ENV === 'dev' && app.use(morgan('dev')) // logs only in dev mode
 app.use(express.json())
 
 app.listen(port, '0.0.0.0', () => {
