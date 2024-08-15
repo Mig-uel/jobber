b778e0ca9f2031560023b8a1a42f43774a561d24
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-14 21:05:03 -0400

    refactor: Update MobileSidebar component to use NavLink for navigation

--- frontend/src/components/MobileSidebar.jsx
+++ frontend/src/components/MobileSidebar.jsx
@@ -1,11 +1,37 @@
 import { useDashboardContext } from '../pages/Dashboard/DashboardLayout'
 import { Wrapper } from '../styled/MobileSidebar'
+import { links } from '../utils/links'
+import { FaTimes } from 'react-icons/fa'
+import Logo from './Logo'
+import { NavLink } from 'react-router-dom'
 
-const SmallSidebar = () => {
+const MobileSidebar = () => {
   const data = useDashboardContext()
-  console.log(data)
 
-  return <Wrapper></Wrapper>
+  return (
+    <Wrapper>
+      <div className='sidebar-container show-sidebar'>
+        <div className='content'>
+          <button type='button' className='close-btn'>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>
+            {links.map((link) => {
+              const { path, text, icon } = link
+              return (
+                <NavLink to={path} key={text} className='nav-link'>
+                  <span className='icon'>{icon}</span> {text}
+                </NavLink>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
-export default SmallSidebar
+export default MobileSidebar
