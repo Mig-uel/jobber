90afaaed850662f102570a5b76751b9f3de8887f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-14 20:04:52 -0400

    refactor: Update MobileSidebar component to use DashboardContext

--- frontend/src/components/MobileSidebar.jsx
+++ frontend/src/components/MobileSidebar.jsx
@@ -1,6 +1,10 @@
+import { useDashboardContext } from '../pages/Dashboard/DashboardLayout'
 import { Wrapper } from '../styled/MobileSidebar'
 
 const SmallSidebar = () => {
+  const data = useDashboardContext()
+  console.log(data)
+
   return <Wrapper></Wrapper>
 }
 
