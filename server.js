962b16b3f7204fbb6b8b691c688687d819a7703c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-15 17:38:04 -0400

    refactor: Update server.js to include MongoDB connection and error handling for improved functionality

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-import express from 'express'
 import morgan from 'morgan'
 import { configDotenv } from 'dotenv'
 configDotenv({})
+import express from 'express'
+import mongoose from 'mongoose'
+import { connectDB } from './utils/db.utils.js'
 import { errorHandler } from './middleware/errorHandler.middleware.js'
 
 // routers
@@ -25,6 +27,13 @@ app.use('*', (req, res) => {
 // custom error handling
 app.use(errorHandler) // triggered by existing controllers (valid routes)
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`SERVER RUNNING ON PORT: ${port}`)
-})
+try {
+  await connectDB()
+  console.log(`MONGODB CONNECTED: ${mongoose.connection.name}`)
+  app.listen(port, '0.0.0.0', () => {
+    console.log(`SERVER RUNNING ON PORT: ${port}`)
+  })
+} catch (error) {
+  console.log(error)
+  process.exit(1)
+}
