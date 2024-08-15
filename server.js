02c395ee02e8fac330ccf205cdd281cafd41df04
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-15 15:53:15 -0400

    refactor: Add jobs router to server.js

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ import morgan from 'morgan'
 import { configDotenv } from 'dotenv'
 configDotenv({})
 
-const port = process.env.PORT || 5100
+// routers
+import jobsRouter from './routes/jobs.route.js'
+
 const app = express()
+const port = process.env.PORT || 5100
 
 // middleware
 process.env.NODE_ENV === 'dev' && app.use(morgan('dev')) // logs only in dev mode
 app.use(express.json())
+
+// routes
+app.use('/api/v1/jobs', jobsRouter)
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`SERVER RUNNING ON PORT: ${port}`)
