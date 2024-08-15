51bc2d0d0a3d916f8252ebd184e8df1db3087a29
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-15 18:07:19 -0400

    refactor: Update errorHandler middleware to handle default error message

--- middleware/errorHandler.middleware.js
+++ middleware/errorHandler.middleware.js
@@ -2,7 +2,7 @@ export const errorHandler = (err, req, res, next) => {
   console.log(err)
 
   const status = err.status || 500
-  const { message } = err
+  const message = err.message || 'Something went wrong...'
 
   return res.status(status).json({ message })
 }
