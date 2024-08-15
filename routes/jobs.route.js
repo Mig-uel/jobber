070f8059ab07eb7250ada0265e3f2e7b002cf1aa
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-15 17:05:34 -0400

    refactor: Update jobs.route.js to use more descriptive function names and improve code organization

--- routes/jobs.route.js
+++ routes/jobs.route.js
@@ -4,18 +4,18 @@ import e, { Router } from 'express'
 import {
   getJobs,
   getJob,
-  editJob,
-  addJob,
+  updateJob,
+  createJob,
   deleteJob,
 } from '../controllers/jobs.controller.js'
 
 // router obj
 const router = Router()
 
-// /api/v1/jobs - GET get all jobs | POST add job
-router.route('/').get(getJobs).post(addJob)
+// /api/v1/jobs - GET get all jobs | POST create job
+router.route('/').get(getJobs).post(createJob)
 
-// /api/v1/jobs/:id - GET get single job | PATCH edit job | DELETE remove job
-router.route('/:id').get(getJob).patch(editJob).delete(deleteJob)
+// /api/v1/jobs/:id - GET get single job | PATCH update job | DELETE remove job
+router.route('/:id').get(getJob).patch(updateJob).delete(deleteJob)
 
 export default router
