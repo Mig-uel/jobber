2e02043d8420b4a18b0904387d551b6e8732556a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-17 17:29:52 -0400

    feat: Add logout route for user authentication
    This code change adds a new route `/api/v1/auth/logout` to the `auth.route.js` file. The route is responsible for logging out the user and is handled by the `logout` controller function. This feature enhances the functionality of the API by providing an endpoint specifically for user logout.

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 
 // controllers
-import { login, register } from '../controllers/auth.controller.js'
+import { login, logout, register } from '../controllers/auth.controller.js'
 import {
   validateLogin,
   validateRegisterInput,
@@ -10,7 +10,13 @@ import {
 // router init
 const router = Router()
 
+// /api/v1/auth/register - POST register user
 router.post('/register', validateRegisterInput, register)
+
+// /api/v1/auth/login - POST login user
 router.post('/login', validateLogin, login)
+
+// /api/v1/auth/logout - GET logout user
+router.get('/logout', logout)
 
 export default router
