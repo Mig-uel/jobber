42be5218b5ec192ec79eae50c3df9dca0b97c0eb
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-16 23:56:10 -0400

    refactor: Add auth controller with register and login methods
    This commit adds an auth controller in the `auth.controller.js` file. The controller includes methods for registering a user and logging in. These methods handle the corresponding API routes for user registration and login. This change improves the codebase by separating the authentication logic into a dedicated controller, enhancing code organization and maintainability.

---
+++ controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import User from '../models/user.model'
+
+/**
+ * @desc REGISTER
+ * @method POST
+ * @path /api/v1/auth/register
+ * @access PUBLIC
+ */
+export const register = async (req, res) => {}
+
+/**
+ * @desc LOGIN
+ * @method POST
+ * @path /api/v1/auth/login
+ * @access PUBLIC
+ */
+export const login = async (req, res) => {}
