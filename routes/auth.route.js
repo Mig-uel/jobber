3e756ae87a351b9f5503fff8d4d55cb2dbb566d8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-17 01:32:34 -0400

    refactor: Add validation middleware for login endpoint
    The code changes include the addition of validation middleware in the `validation.middleware.js` file to validate the input fields during the login process. This ensures that the user input meets the required criteria before proceeding with the login functionality. The middleware includes validation rules for the email and password fields. This change improves the codebase by enhancing the security and reliability of the login endpoint.

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,12 +2,15 @@ import { Router } from 'express'
 
 // controllers
 import { login, register } from '../controllers/auth.controller.js'
-import { validateRegisterInput } from '../middleware/validation.middleware.js'
+import {
+  validateLogin,
+  validateRegisterInput,
+} from '../middleware/validation.middleware.js'
 
 // router init
 const router = Router()
 
 router.post('/register', validateRegisterInput, register)
-router.post('/login', login)
+router.post('/login', validateLogin, login)
 
 export default router
