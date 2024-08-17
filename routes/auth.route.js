ac776e17c1ee9feb3c0102e98b5fd020f5b0dd5f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-17 00:29:59 -0400

    refactor: Add validation middleware for user registration input
    This commit adds a validation middleware in the `validation.middleware.js` file to validate the input fields during user registration. It includes validation rules for the first name, last name, email, and password fields. This change improves the codebase by ensuring that the user input meets the required criteria before proceeding with the registration process.

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,11 +2,12 @@ import { Router } from 'express'
 
 // controllers
 import { login, register } from '../controllers/auth.controller.js'
+import { validateRegisterInput } from '../middleware/validation.middleware.js'
 
 // router init
 const router = Router()
 
-router.post('/register', register)
+router.post('/register', validateRegisterInput, register)
 router.post('/login', login)
 
 export default router
