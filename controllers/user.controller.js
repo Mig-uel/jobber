be88e9d4058bf07ce86753d69dcf6149cf973ea9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-17 17:26:50 -0400

    feat: Add getUser controller function for retrieving user profile
    This code change adds a new controller function, getUser, to the user.controller.js file. The function is responsible for retrieving the user's profile and is accessed via the GET method at the /api/v1/users endpoint. This feature enhances the functionality of the API by providing an endpoint for users to retrieve their profile information.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,20 @@
+import User from '../models/user.model.js'
+import Job from '../models/job.model.js'
+
+/**
+ * @desc GET USERS PROFILE
+ * @method GET
+ * @path /api/v1/users
+ * @access PRIVATE
+ */
+export const getUser = async (req, res) => {
+  const { id } = req.user
+
+  const user = await User.findById(id).select('-password')
+
+  return res.status(200).json({ user })
+}
+
 /**
  * @desc UPDATE USERS PROFILE
  * @method PATCH
