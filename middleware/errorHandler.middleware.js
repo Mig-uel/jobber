89b52c2beb5b15090b3249b9e6608c5a83a7c26d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-16 21:33:37 -0400

    Remove unnecessary code in errorHandler.middleware.js to improve code readability and maintainability. The code for handling invalid ObjectIds is no longer needed since express validator is now being used. This change simplifies the middleware and reduces the risk of inconsistencies in error handling.

--- middleware/errorHandler.middleware.js
+++ middleware/errorHandler.middleware.js
@@ -1,16 +1,15 @@
 export const errorHandler = (err, req, res, next) => {
   console.log(err)
 
-  let message
-  let status = err.status || 500
-  let stack = process.env.NODE_ENV === 'dev' ? err.stack : '🥞'
+  const message = err.message || 'Something went wrong...'
+  const status = err.status || 500
+  const stack = process.env.NODE_ENV === 'dev' ? err.stack : '🥞'
 
-  if (err.kind === 'ObjectId') {
-    message = 'Invalid ID'
-    return res.status(status).json({ message, status, stack })
-  }
-
-  message = err.message || 'Something went wrong...'
+  // no longer need, using express validator
+  // if (err.kind === 'ObjectId') {
+  //   message = 'Invalid ID'
+  //   return res.status(status).json({ message, status, stack })
+  // }
 
   return res.status(status).json({ message, status, stack })
 }
