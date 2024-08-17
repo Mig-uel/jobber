0e80af97dff80977a22455a29f0f7328ade2455e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-17 03:39:50 -0400

    refactor: Add authenticateUser middleware for authentication
    The code changes in the auth.middleware.js file add a new middleware function called authenticateUser. This middleware checks if the request contains a token in the cookies and verifies the token using the verifyToken function from the jwt.utils.js file. If the token is valid, it sets the user ID and role in the request object. If the token is invalid, it throws an UnauthenticatedError. This refactor enhances the security of the API by ensuring that only authenticated users can access certain routes.

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,4 +1,5 @@
 import { UnauthenticatedError } from '../utils/errors.utils.js'
+import { verifyToken } from '../utils/jwt.utils.js'
 
 export const isAuth = async (req, res, next) => {
   const { token } = req.cookies
@@ -6,4 +7,19 @@ export const isAuth = async (req, res, next) => {
   if (!token) throw new UnauthenticatedError('Not authenticated')
 
   next()
+}
+
+export const authenticateUser = async (req, res, next) => {
+  const { token } = req.cookies
+
+  try {
+    const { id, role } = verifyToken(token)
+
+    req.id = id
+    req.role = role
+
+    next()
+  } catch (error) {
+    throw new UnauthenticatedError('Authentication failed')
+  }
 }
