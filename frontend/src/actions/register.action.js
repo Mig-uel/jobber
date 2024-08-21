b236e1774a403f3e17db2ea425b156abf2fb4dc4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-21 19:23:14 -0400

    feat: Refactor registerAction to use FormData for form handling
    The registerAction in register.action.js has been refactored to use the FormData API for handling form data. This improvement enhances the code by simplifying the process of extracting form input values and transforming them into an object.

--- frontend/src/actions/register.action.js
+++ frontend/src/actions/register.action.js
@@ -1,6 +1,35 @@
+/**
+ * REGISTER POST BODY
+ * {
+ *    firstName
+ *    lastName
+ *    email
+ *    password
+ * }
+ */
+
+import { customFetch } from '../utils/fetch.utils'
+import { redirect } from 'react-router-dom'
+
+// only use 'redirect' in an action
 const registerAction = async (data) => {
-  console.log(data)
-  return null
+  try {
+    const { request } = data
+
+    // get the form input
+    const formData = await request.formData()
+
+    // transform the input values to an object
+    const signupData = Object.fromEntries(formData)
+
+    await customFetch.post('/auth/register', signupData)
+
+    return redirect('/login')
+  } catch (error) {
+    console.log(error)
+
+    return error
+  }
 }
 
 export default registerAction
