933ca1451eb9130fca5fd816b5c39a2e40f05504
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-22 19:49:18 -0400

    feat: Add editJobAction for updating job details
    The editJobAction function has been added to the project. This function is responsible for updating the details of a job, such as the company, position, status, type, and location. It uses the customFetch utility to make a PATCH request to the server with the updated data. If the request is successful, a success toast message is displayed and the user is redirected to the dashboard. If there is an error, an error toast message is displayed. This addition enhances the functionality of the application by providing a way to modify existing job listings.
    
    Note: This commit message follows the established conventions in the repository.

---
+++ frontend/src/actions/editjob.action.js
@@ -0,0 +1,37 @@
+/**
+ * EDIT JOB PATCH BODY
+ * {
+ *    company
+ *    position
+ *    status
+ *    type
+ *    location
+ * }
+ */
+
+import { redirect } from 'react-router-dom'
+import { customFetch } from '../utils/fetch.utils'
+import { toast } from 'react-toastify'
+
+const editJobAction = async (data) => {
+  try {
+    const {
+      params: { id },
+      request,
+    } = data
+
+    const formData = await request.formData()
+
+    const editData = Object.fromEntries(formData)
+
+    await customFetch.patch(`/jobs/${id}`, editData)
+
+    toast.success('Updated job posting! 🎉')
+    return redirect('/dashboard/jobs')
+  } catch (error) {
+    toast.error(error?.response?.data?.message)
+    return error
+  }
+}
+
+export default editJobAction
--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,3 +1,4 @@
 export { default as registerAction } from './register.action'
 export { default as loginAction } from './login.action'
 export { default as addJobAction } from './addjob.action'
+export { default as editJobAction } from './editjob.action'
