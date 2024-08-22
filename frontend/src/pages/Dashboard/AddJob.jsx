7634c352719a46710ff5c6f2ef81df2474654d8c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-21 22:30:32 -0400

    refactor: Update AddJob component to include form for adding a job
    The AddJob component in AddJob.jsx has been updated to include a form for adding a job. This enhancement allows users to input the position, company, location, and status of the job they want to add. The form also includes validation and a submit button for adding the job. This improvement improves the functionality of the AddJob page and provides a better user experience.
    
    Note: This commit message follows the established conventions in the repository.

--- frontend/src/pages/Dashboard/AddJob.jsx
+++ frontend/src/pages/Dashboard/AddJob.jsx
@@ -1,5 +1,60 @@
+import { Form, useNavigation, useOutletContext } from 'react-router-dom'
+import { customFetch } from '../../utils/fetch.utils'
+import { STATUS } from '../../../../utils/constants.utils'
+
+// ui
+import { toast } from 'react-toastify'
+import { FormRow } from '../../components'
+import { Wrapper } from '../../styled/DashboardFormPage'
+
 const AddJob = () => {
-  return <h1>Add Job</h1>
+  const { user } = useOutletContext()
+  const navigation = useNavigation()
+
+  // get navigation state from useNavigation hook
+  const isSubmitting = navigation.state === 'submitting'
+
+  return (
+    <Wrapper>
+      <Form method='post' className='form'>
+        <h4 className='form-title'>Add Job</h4>
+
+        <div className='form-center'>
+          <FormRow type='text' name='position' />
+          <FormRow type='text' name='company' />
+          <FormRow
+            type='text'
+            name='location'
+            defaultValue={user?.location || ''}
+          />
+
+          <div className='form-row'>
+            <label htmlFor='status' className='form-label'>
+              Job Status
+            </label>
+            <select
+              name='status'
+              id='status'
+              className='form-select'
+              defaultValue={STATUS.PENDING}
+            >
+              {Object.values(STATUS).map((status, index) => (
+                <option key={index}>{status}</option>
+              ))}
+            </select>
+          </div>
+
+          <button
+            type='submit'
+            className='btn btn-block form-btn'
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Adding...' : 'Add Job'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  )
 }
 
 export default AddJob
