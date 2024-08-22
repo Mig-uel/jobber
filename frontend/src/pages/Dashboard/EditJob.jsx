fdbe37dc1f2ac39c5f4638c53d2c5f4fa2bf1a2f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-22 19:50:11 -0400

    feat: Improve EditJob component with error handling and input fields

--- frontend/src/pages/Dashboard/EditJob.jsx
+++ frontend/src/pages/Dashboard/EditJob.jsx
@@ -1,25 +1,70 @@
 import { useEffect } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import {
+  Form,
+  useActionData,
+  useLocation,
+  useNavigate,
+  useNavigation,
+  useParams,
+} from 'react-router-dom'
+import { STATUS, TYPE } from '../../../../utils/constants.utils'
+
+// ui
+import { Wrapper } from '../../styled/DashboardFormPage'
+import { FormRow, FormRowSelect } from '../../components'
 
 const EditJob = () => {
+  let { id } = useParams()
   const { state } = useLocation()
-  const navigate = useNavigate()
+  const error = useActionData()
 
-  console.log(state)
+  const navigate = useNavigate()
+  const navigation = useNavigation()
+
+  const isSubmitting = navigation.state === 'submitting'
 
   useEffect(() => {
-    const error = new Error('Invalid state or ID!')
-    error.status = 404
+    if (!state) {
+      return navigate(`/dashboard/jobs`)
+    }
 
-    if (!state) throw error
-  }, [navigate, state])
+    if (state && error) {
+      return navigate(`/dashboard/edit/${id}`, { state })
+    }
+  }, [navigate, state, id, error])
 
   return (
-    <>
-      <h1>Edit Job</h1>
+    <Wrapper>
+      <Form method='patch' className='form'>
+        <h4 className='form-title'>Edit Job Posting</h4>
 
-      <input type='text' defaultValue={state?.company} />
-    </>
+        <div className='form-center'>
+          <FormRow type='text' name='position' defaultValue={state?.position} />
+          <FormRow type='text' name='company' defaultValue={state?.company} />
+          <FormRow type='text' name='location' defaultValue={state?.location} />
+          <FormRowSelect
+            name='status'
+            labelText='Job Status'
+            defaultValue={state?.status}
+            list={Object.values(STATUS)}
+          />
+          <FormRowSelect
+            name='type'
+            labelText='Job Type'
+            defaultValue={state?.type}
+            list={Object.values(TYPE)}
+          />
+
+          <button
+            type='submit'
+            className='btn btn-block form-btn'
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Updating...' : 'Update'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
   )
 }
 
