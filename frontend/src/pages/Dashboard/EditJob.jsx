2f5248eed3c52154ebe5bef0ad632087cfd6a3fc
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-22 18:53:12 -0400

    feat: Improve EditJob component with error handling and input fields
    The EditJob component has been updated to include error handling for invalid state or ID and to display input fields for editing job details. These enhancements improve the functionality and user experience of the application by providing better error feedback and a convenient way to modify job listings.
    
    Note: This commit message follows the established conventions in the repository.

--- frontend/src/pages/Dashboard/EditJob.jsx
+++ frontend/src/pages/Dashboard/EditJob.jsx
@@ -1,5 +1,26 @@
+import { useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+
 const EditJob = () => {
-  return <></>
+  const { state } = useLocation()
+  const navigate = useNavigate()
+
+  console.log(state)
+
+  useEffect(() => {
+    const error = new Error('Invalid state or ID!')
+    error.status = 404
+
+    if (!state) throw error
+  }, [navigate, state])
+
+  return (
+    <>
+      <h1>Edit Job</h1>
+
+      <input type='text' defaultValue={state?.company} />
+    </>
+  )
 }
 
 export default EditJob
