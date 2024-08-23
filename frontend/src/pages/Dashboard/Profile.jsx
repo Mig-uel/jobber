edf4ed5e554d66d36f8e79b5c9744c73e127e681
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-23 09:57:19 -0400

    refactor: Update Profile page to include avatar upload functionality
    The Profile page in the frontend/src/pages/Dashboard/Profile.jsx file has been updated to include the ability for users to upload an avatar image. The form now includes an input field for selecting an image file, with a maximum size of 0.5MB. This refactor enhances the user experience by allowing users to personalize their profile with an avatar image.
    
    Note: This commit message follows the established conventions in the repository.

--- frontend/src/pages/Dashboard/Profile.jsx
+++ frontend/src/pages/Dashboard/Profile.jsx
@@ -1,5 +1,62 @@
+import { Form, useNavigation, useOutletContext } from 'react-router-dom'
+import { FormRow } from '../../components'
+import { Wrapper } from '../../styled/DashboardFormPage'
+
 const Profile = () => {
-  return <></>
+  const {
+    user: { firstName, lastName, email, location },
+  } = useOutletContext()
+
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === 'submitting'
+
+  return (
+    <Wrapper>
+      <Form method='patch' className='form' encType='multipart/form-data'>
+        <h4 className='form-title'>Profile</h4>
+        <div className='form-center'>
+          <div className='form-row'>
+            <label htmlFor='avatar' className='form-label'>
+              Select an image file (max 0.5MB)
+            </label>
+            <input
+              type='file'
+              name='avatar'
+              id='avatar'
+              className='form-input'
+              accept='image/*'
+            />
+          </div>
+
+          <FormRow
+            type='text'
+            name='firstName'
+            defaultValue={firstName}
+            labelText='First Name'
+          />
+
+          <FormRow
+            type='text'
+            name='lastName'
+            defaultValue={lastName}
+            labelText='Last Name'
+          />
+
+          <FormRow type='text' name='email' defaultValue={email} />
+
+          <FormRow type='text' name='location' defaultValue={location} />
+
+          <button
+            type='submit'
+            className='btn btn-block form-btn'
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Updating...' : 'Update'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  )
 }
 
 export default Profile
