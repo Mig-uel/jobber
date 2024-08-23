91849e9402d9b3a7380619ef78d8de2c8f446709
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-22 20:30:38 -0400

    feat: Add deleteJobLoader for deleting job postings
    A new loader, deleteJobLoader, has been added to handle the deletion of job postings. This loader makes a DELETE request to the server to delete the job with the specified ID. If the deletion is successful, a success toast message is displayed and the user is redirected to the jobs dashboard. If there is an error, an error toast message is displayed. This addition enhances the functionality of the application by providing a way to delete job postings.
    
    Note: This commit message follows the established conventions in the repository.

---
+++ frontend/src/loaders/deletejob.loader.js
@@ -0,0 +1,23 @@
+/**
+ * DELETE JOB
+ */
+
+import { redirect } from 'react-router-dom'
+import { customFetch } from '../utils/fetch.utils'
+import { toast } from 'react-toastify'
+
+const deleteJobLoader = async ({ params }) => {
+  try {
+    const { id } = params
+
+    await customFetch.delete(`/jobs/${id}`)
+
+    toast.success('Deleted job posting! 🗑️')
+    return redirect('/dashboard/jobs')
+  } catch (error) {
+    toast.error(error?.response?.data?.message)
+    return redirect('/dashboard/jobs')
+  }
+}
+
+export default deleteJobLoader
--- frontend/src/loaders/index.js
+++ frontend/src/loaders/index.js
@@ -1,3 +1,4 @@
 export { default as dashboardLoader } from './dashboard.loader'
 export { default as jobsLoader } from './jobs.loader'
 export { default as editJobLoader } from './editjob.loader'
+export { default as deleteJobLoader } from './deleteJob.loader'
