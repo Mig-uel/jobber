186b3dfc7fdc481ae7660a869a8f482b970b572f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-22 21:43:01 -0400

    refactor: Update NavLinks component to conditionally render admin link
    The NavLinks component has been updated to conditionally render the admin link based on the user's role. If the user is an admin, the admin link will be displayed in the navigation bar. This refactor improves the user experience by providing a more tailored navigation menu for admins.
    
    Note: This commit message follows the established conventions in the repository.

--- frontend/src/components/NavLinks.jsx
+++ frontend/src/components/NavLinks.jsx
@@ -4,23 +4,42 @@ import { links } from '../utils/links'
 import PropTypes from 'prop-types'
 
 const NavLinks = ({ isDesktopSidebar }) => {
-  const { toggleSidebar } = useDashboardContext()
+  const { toggleSidebar, user } = useDashboardContext()
+
+  const isAdmin = user.role === 'admin'
 
   return (
     <div className='nav-links'>
       {links.map((link) => {
         const { path, text, icon } = link
-        return (
-          <NavLink
-            to={path}
-            key={text}
-            className='nav-link'
-            onClick={isDesktopSidebar ? null : toggleSidebar}
-            end
-          >
-            <span className='icon'>{icon}</span> {text}
-          </NavLink>
-        )
+
+        if (path === 'admin') {
+          if (isAdmin) {
+            return (
+              <NavLink
+                to={path}
+                key={text}
+                className='nav-link'
+                onClick={isDesktopSidebar ? null : toggleSidebar}
+                end
+              >
+                <span className='icon'>{icon}</span> {text}
+              </NavLink>
+            )
+          } else return
+        } else {
+          return (
+            <NavLink
+              to={path}
+              key={text}
+              className='nav-link'
+              onClick={isDesktopSidebar ? null : toggleSidebar}
+              end
+            >
+              <span className='icon'>{icon}</span> {text}
+            </NavLink>
+          )
+        }
       })}
     </div>
   )
