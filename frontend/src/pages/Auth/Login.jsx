d3d39545a6b931d2d77996cad7649b1ac2a64e51
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-23 14:48:27 -0400

    feat: Add login demo user functionality
    This commit adds the functionality to login as a demo user in the Login page. When the "Explore the App" button is clicked, the loginDemoUser function is triggered. It sends a POST request to the '/auth/login' endpoint with the demo user credentials. If successful, it displays a success toast message and navigates to the dashboard page. If there is an error, it displays an error toast message. This enhancement improves the user experience by allowing users to easily explore the app without creating an account.
    
    Note: This commit message follows the established conventions in the repository.

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,9 +1,26 @@
-import { Form, Link } from 'react-router-dom'
+import { Form, Link, useNavigate } from 'react-router-dom'
 
 import { Wrapper } from '../../styled/RegisterAndLogin'
 import { Logo, FormRow, SubmitButton } from '../../components'
+import { customFetch } from '../../utils/fetch.utils'
+import { toast } from 'react-toastify'
 
 const Login = () => {
+  const navigate = useNavigate()
+
+  const loginDemoUser = async () => {
+    try {
+      const data = { email: 'john@demo.com', password: 'johndoe123' }
+
+      await customFetch.post('/auth/login', data)
+
+      toast.success(`Take a test drive`)
+      return navigate('/dashboard')
+    } catch (error) {
+      toast.error(error?.response?.data?.message)
+    }
+  }
+
   return (
     <Wrapper>
       <Form className='form' method='post'>
@@ -15,7 +32,7 @@ const Login = () => {
 
         <SubmitButton />
 
-        <button type='button' className='btn btn-block'>
+        <button type='button' className='btn btn-block' onClick={loginDemoUser}>
           Explore the App
         </button>
 
