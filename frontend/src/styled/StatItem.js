b6fa929921a276d66cb314b7819146fa0aa8733a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-22 22:12:12 -0400

    refactor: Rename StatItem.js to styled/StatItem.js
    The StatItem.js file has been renamed to styled/StatItem.js. This refactor improves the organization and structure of the code by moving the file to the appropriate directory.
    
    Note: This commit message follows the established conventions in the repository.

--- frontend/src/styled/StatItem.js
+++ frontend/src/styled/StatItem.js
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-const Wrapper = styled.article`
+export const Wrapper = styled.article`
   padding: 2rem;
   background: var(--background-secondary-color);
-  border-bottom: 5px solid ${(props) => props.color};
+  border-bottom: 5px solid ${(props) => props.props.color};
   border-radius: var(--border-radius);
 
   header {
@@ -15,7 +15,7 @@ const Wrapper = styled.article`
     display: block;
     font-weight: 700;
     font-size: 50px;
-    color: ${(props) => props.color};
+    color: ${(props) => props.props.color};
     line-height: 2;
   }
   .title {
@@ -29,16 +29,14 @@ const Wrapper = styled.article`
   .icon {
     width: 70px;
     height: 60px;
-    background: ${(props) => props.bcg};
+    background: ${(props) => props.props.bcg};
     border-radius: var(--border-radius);
     display: flex;
     align-items: center;
     justify-content: center;
     svg {
       font-size: 2rem;
-      color: ${(props) => props.color};
+      color: ${(props) => props.props.color};
     }
   }
-`;
-
-export default Wrapper;
+`
