5698e765167964f2e7c3df978eb991d9094b2a01
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-24 15:15:17 -0400

    feat: Update jobsLoader to include request parameters in API call
    This commit updates the jobsLoader function in the frontend/src/loaders/jobs.loader.js file to include the request parameters in the API call to '/jobs'. The URL is parsed to extract the search parameters, which are then passed as query parameters in the API call. This enhancement allows for more customized and filtered job data to be fetched based on the user's request.
    
    Note: This commit message follows the established conventions in the repository.

--- frontend/src/loaders/jobs.loader.js
+++ frontend/src/loaders/jobs.loader.js
@@ -1,9 +1,14 @@
 import { customFetch } from '../utils/fetch.utils'
 import { toast } from 'react-toastify'
 
-const jobsLoader = async () => {
+const jobsLoader = async ({ request }) => {
   try {
-    const { data } = await customFetch.get('/jobs')
+    const { url } = request
+
+    const searchParams = new URL(url).searchParams.entries()
+    const params = Object.fromEntries([...searchParams])
+
+    const { data } = await customFetch.get('/jobs', { params })
 
     return data
   } catch (error) {
