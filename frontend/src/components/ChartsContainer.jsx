8bc5d439ddb3c07c03d21dda3f4e77dd0d9f6670
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-24 12:46:32 -0400

    feat: Update ChartsContainer to accept data prop and toggle between AreaChart and BarChart
    This commit updates the ChartsContainer component in frontend/src/components/ChartsContainer.jsx to accept a data prop. It also adds functionality to toggle between displaying an AreaChart and a BarChart based on the value of the barChart state. This improvement enhances the component by making it more flexible and allowing it to render different types of charts based on the provided data.
    
    Note: This commit message follows the established conventions in the repository.

--- frontend/src/components/ChartsContainer.jsx
+++ frontend/src/components/ChartsContainer.jsx
@@ -1,5 +1,27 @@
-const ChartsContainer = () => {
-  return <></>
+import { useState } from 'react'
+import AreaChart from './AreaChart'
+import BarChart from './BarChart'
+import { Wrapper } from '../styled/ChartsContainer'
+
+import PropType from 'prop-types'
+
+const ChartsContainer = ({ data }) => {
+  const [barChart, setBarChart] = useState(true)
+
+  return (
+    <Wrapper>
+      <h4>Montly Applications</h4>
+      <button type='button' onClick={() => setBarChart((prev) => !prev)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  )
 }
 
 export default ChartsContainer
+
+ChartsContainer.propTypes = {
+  data: PropType.object.isRequired,
+}
