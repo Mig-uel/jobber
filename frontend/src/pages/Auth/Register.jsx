6e01c24619e5b8a3d1a8f84617d92df4b7daa790
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-27 18:25:40 -0400

    refactor: Update Register component form inputs
    Refactor the Register component in frontend/src/pages/Auth/Register.jsx to remove default values from the form inputs. This change improves the user experience by allowing users to enter their own values for first name, last name, location, email, and password.
    
    Note: This commit message follows the established conventions in the repository.

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -10,21 +10,11 @@ const Register = () => {
         <Logo />
         <h4>Register</h4>
 
-        <FormRow
-          type='text'
-          name='firstName'
-          defaultValue='John'
-          labelText='First Name'
-        />
-        <FormRow
-          type='text'
-          name='lastName'
-          defaultValue='Smith'
-          labelText='Last Name'
-        />
-        <FormRow type='text' name='location' defaultValue='Brooklyn' />
-        <FormRow type='email' name='email' defaultValue='john@email.com' />
-        <FormRow type='password' name='password' defaultValue='123456' />
+        <FormRow type='text' name='firstName' labelText='First Name' />
+        <FormRow type='text' name='lastName' labelText='Last Name' />
+        <FormRow type='text' name='location' />
+        <FormRow type='email' name='email' />
+        <FormRow type='password' name='password' />
 
         <SubmitButton />
 
