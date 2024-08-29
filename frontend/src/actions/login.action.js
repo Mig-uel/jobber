dcd662c4db8bd140c6053b3d1ebdd74547ba6f2a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-08-29 06:22:40 -0400

    refactor: Update loginAction to use queryClient for data loading

--- frontend/src/actions/login.action.js
+++ frontend/src/actions/login.action.js
@@ -10,7 +10,7 @@ import { customFetch } from '../utils/fetch.utils'
 import { redirect } from 'react-router-dom' // only use 'redirect' in actions
 import { toast } from 'react-toastify'
 
-const loginAction = async (data) => {
+const loginAction = (queryClient) => async (data) => {
   try {
     const { request } = data
 
@@ -22,6 +22,9 @@ const loginAction = async (data) => {
 
     // send post request and store request to use in toast message
     const user = await customFetch.post('/auth/login', loginData)
+
+    // invalidate query cache
+    queryClient.invalidateQueries()
 
     toast.success(`Welcome back, ${user.data.name}!`)
     return redirect('/dashboard')
