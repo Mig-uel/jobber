e8f729c2d2380a496dbf8284aba12a3bb3632f09
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-20 17:57:42 -0500

    fix: Remove unused exports from components index

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,2 @@
-export { default as LinksDropdown } from './LinksDropdown'
 export { default as Navbar } from './Navbar'
 export { default as Sidebar } from './Sidebar'
-export { default as ThemeToggle } from './ThemeToggle'
