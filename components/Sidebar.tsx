cc2f295fc7bcbbd351c54fdd6e3fd2731f76bf58
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-20 18:19:38 -0500

    feat: Enhance Sidebar component with logo and dynamic navigation links

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,33 @@
+'use client'
+
+import Logo from '@/assets/logo.svg'
+import { navLinks } from '@/utils/links'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { Button } from './ui/button'
+
 export default function Sidebar() {
-  return <div>Sidebar</div>
+  const pathname = usePathname()
+
+  return (
+    <aside className='py-4 px-8 bg-muted h-full'>
+      <Image src={Logo} alt='logo' className='mx-auto' />
+
+      <div className='flex flex-col mt-20 gap-y-4'>
+        {navLinks.map((link) => (
+          <Button
+            asChild
+            key={link.href}
+            variant={pathname === link.href ? 'default' : 'link'}
+          >
+            <Link href={link.href} className='flex items-center gap-x-2'>
+              {link.icon}
+              <span className='capitalize'>{link.label}</span>
+            </Link>
+          </Button>
+        ))}
+      </div>
+    </aside>
+  )
 }
