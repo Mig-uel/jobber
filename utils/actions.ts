0e8780d4d2ab6f39c2dcd1030c1562312eabcfca
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-21 08:46:23 -0500

    feat: Implement createJobAction for job creation with authentication and validation

---
+++ utils/actions.ts
@@ -0,0 +1,37 @@
+'use server'
+
+import prisma from './db'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+import { createAndEditJobSchema, CreateAndEditJobType, JobType } from './types'
+
+function authenticateAndRedirect(): string {
+  const { userId } = auth()
+
+  if (!userId) redirect('/')
+
+  return userId
+}
+
+export async function createJobAction(
+  values: CreateAndEditJobType
+): Promise<JobType | null> {
+  const clerkId = authenticateAndRedirect()
+
+  try {
+    createAndEditJobSchema.parse(values)
+
+    const job: JobType = await prisma.job.create({
+      data: {
+        ...values,
+        clerkId,
+      },
+    })
+
+    return job
+  } catch (error) {
+    if (error instanceof Error) console.log(error.message)
+
+    return null
+  }
+}
