9d450e3c77b4bba6af6d74da02f7d211bf3cface
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-21 12:05:57 -0500

    fix: Add margin to loading and no jobs found messages for better spacing

--- components/JobsList.tsx
+++ components/JobsList.tsx
@@ -25,9 +25,9 @@ export default function JobsList() {
 
   const jobs = data?.jobs || []
 
-  if (isPending) return <h2 className='text-xl'>Loading jobs...</h2>
+  if (isPending) return <h2 className='text-xl mt-8'>Loading jobs...</h2>
 
-  if (!jobs.length) return <h2 className='text-xl'>No jobs found :(</h2>
+  if (!jobs.length) return <h2 className='text-xl mt-8'>No jobs found :(</h2>
 
   return (
     <>
