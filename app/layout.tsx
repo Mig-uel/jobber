e4c203391065adcde488e04d7d39d2f3923e25f1
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-20 19:17:38 -0500

    feat: Wrap children in Providers component for improved client-side rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import Providers from './_providers'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,11 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang='en'>
+      <html lang='en' suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+          <Providers>{children}</Providers>
         </body>
       </html>
     </ClerkProvider>
