05b81df09dc7be3f47f43d50cb29119265e83c31
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-20 19:10:16 -0500

    fix: Wrap LinksDropdown component in a div for proper layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,9 @@ import { UserButton } from '@clerk/nextjs'
 export default function Navbar() {
   return (
     <nav className='bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between'>
-      <LinksDropdown />
+      <div>
+        <LinksDropdown />
+      </div>
 
       <div className='flex items-center gap-x-4'>
         <ThemeToggle />
