6c4daf3139334d52daf1811a9778eb2746e76153
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-21 10:58:43 -0500

    feat: Implement SearchForm component with job search functionality and status filter

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,3 +1,54 @@
+'use client'
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { Input } from './ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select'
+import { JobStatus } from '@/utils/types'
+import { Button } from './ui/button'
+
 export default function SearchForm() {
-  return <div>SearchForm</div>
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+
+    const job = Object.fromEntries(formData)
+
+    console.log(job)
+  }
+
+  return (
+    <form
+      className='bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg'
+      onSubmit={handleSubmit}
+    >
+      <Input type='text' placeholder='Search jobs' name='search' />
+
+      <Select name='jobStatus' defaultValue='all'>
+        <SelectTrigger>
+          <SelectValue />
+        </SelectTrigger>
+
+        <SelectContent>
+          {['all', ...Object.values(JobStatus)].map((jobStatus) => (
+            <SelectItem key={jobStatus} value={jobStatus}>
+              {jobStatus}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+
+      <Button type='submit'>Search</Button>
+    </form>
+  )
 }
