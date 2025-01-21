8239aa1629a8e57349e01d55b0bd33965326e585
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-20 19:09:32 -0500

    feat: Implement LinksDropdown component with dynamic navigation links

--- components/LinksDropdown.tsx
+++ components/LinksDropdown.tsx
@@ -1,3 +1,40 @@
+'use client'
+
+import { AlignLeft } from 'lucide-react'
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import { navLinks } from '@/utils/links'
+import Link from 'next/link'
+import { useState } from 'react'
+
 export default function LinksDropdown() {
-  return <div>LinksDropdown</div>
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className='lg:hidden'>
+        <Button variant='outline' size='icon'>
+          <AlignLeft />
+          <span className='sr-only'>Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        className='w-52 lg:hidden'
+        align='start'
+        sideOffset={25}
+      >
+        {navLinks.map((link) => (
+          <DropdownMenuItem key={link.href} asChild>
+            <Link href={link.href} className='flex items-center gap-x-2'>
+              {link.icon} <span className='capitalize'>{link.label}</span>
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 }
