8cbf006cb136e0a455dba22ff692ce90beb713a4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-21 11:23:50 -0500

    feat: Refactor SearchForm component to enhance UI and improve search functionality

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -11,44 +11,76 @@ import {
 } from './ui/select'
 import { JobStatus } from '@/utils/types'
 import { Button } from './ui/button'
+import { Label } from './ui/label'
 
 export default function SearchForm() {
   const router = useRouter()
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
+  const search = searchParams.get('search') || ''
+  const jobStatus = searchParams.get('jobStatus') || 'all'
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
     const formData = new FormData(e.currentTarget)
 
-    const job = Object.fromEntries(formData)
+    const search = formData.get('search') as string
+    const jobStatus = formData.get('jobStatus') as string
 
-    console.log(job)
+    // construct search params
+    const params = new URLSearchParams()
+
+    params.set('search', search)
+    params.set('jobStatus', jobStatus)
+
+    return router.push(`${pathname}?${params.toString()}`)
   }
 
   return (
-    <form
-      className='bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg'
-      onSubmit={handleSubmit}
-    >
-      <Input type='text' placeholder='Search jobs' name='search' />
+    <div className='bg-muted rounded-lg p-8'>
+      <h2 className='text-4xl font-semibold'>Search</h2>
 
-      <Select name='jobStatus' defaultValue='all'>
-        <SelectTrigger>
-          <SelectValue />
-        </SelectTrigger>
+      <form
+        className='my-6 grid sm:grid-cols-2 md:grid-cols-3 gap-4 items-end'
+        onSubmit={handleSubmit}
+      >
+        <div>
+          <Label htmlFor='search' className='font-semibold'>
+            Search Term
+          </Label>
+          <Input
+            id='search'
+            type='text'
+            placeholder='Search jobs'
+            name='search'
+            defaultValue={search}
+          />
+        </div>
 
-        <SelectContent>
-          {['all', ...Object.values(JobStatus)].map((jobStatus) => (
-            <SelectItem key={jobStatus} value={jobStatus}>
-              {jobStatus}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
+        <div>
+          <Label htmlFor='jobStatus' className='font-semibold'>
+            Job Status
+          </Label>
 
-      <Button type='submit'>Search</Button>
-    </form>
+          <Select name='jobStatus' defaultValue={jobStatus}>
+            <SelectTrigger id='jobStatus'>
+              <SelectValue />
+            </SelectTrigger>
+
+            <SelectContent>
+              {['all', ...Object.values(JobStatus)].map((jobStatus) => (
+                <SelectItem key={jobStatus} value={jobStatus}>
+                  {jobStatus}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+
+        <Button type='submit'>Search</Button>
+      </form>
+    </div>
   )
 }
