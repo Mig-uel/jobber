0b90f9481e148575cff2af84865997870f42007c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-21 11:51:15 -0500

    feat: Enhance JobCard component to display job details with icons and badges

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,4 +1,10 @@
 import type { JobType } from '@/utils/types'
+import { Briefcase, CalendarDays, MapPin, RadioTower } from 'lucide-react'
+import Link from 'next/link'
+import DeleteJobButton from './DeleteJobButton'
+import JobInfo from './JobInfo'
+import { Badge } from './ui/badge'
+import { Button } from './ui/button'
 import {
   Card,
   CardContent,
@@ -8,11 +14,10 @@ import {
   CardTitle,
 } from './ui/card'
 import { Separator } from './ui/separator'
-import { Button } from './ui/button'
-import Link from 'next/link'
-import DeleteJobButton from './DeleteJobButton'
 
 export default function JobCard({ job }: { job: JobType }) {
+  const date = new Date(job.createdAt).toLocaleDateString()
+
   return (
     <Card className='bg-muted'>
       <CardHeader>
@@ -22,7 +27,18 @@ export default function JobCard({ job }: { job: JobType }) {
 
       <Separator />
 
-      <CardContent></CardContent>
+      <CardContent className='mt-4 grid grid-cols-2 gap-4 capitalize'>
+        <JobInfo icon={<Briefcase />} text={job.mode} />
+        <JobInfo icon={<MapPin />} text={job.location} />
+        <JobInfo icon={<CalendarDays />} text={date} />
+
+        <Badge className='w-32 justify-center rounded-full'>
+          <JobInfo
+            icon={<RadioTower className='w-4 h-4' />}
+            text={job.status}
+          />
+        </Badge>
+      </CardContent>
 
       <CardFooter className='flex gap-4'>
         <Button asChild>
