d3441641880e5deaa3e94873e2c04ef5f4eeced1
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-21 10:39:06 -0500

    feat: Enhance job dashboard page with data fetching and component integration

--- app/(dashboard)/jobs/page.tsx
+++ app/(dashboard)/jobs/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <div>Page</div>
+import { JobsList, SearchForm } from '@/components/index'
+import { getAllJobs } from '@/utils/actions'
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query'
+
+export default async function Page() {
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery({
+    queryKey: ['jobs', '', 'all', 1],
+
+    queryFn: () => getAllJobs({}),
+  })
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <SearchForm />
+
+      <JobsList />
+    </HydrationBoundary>
+  )
 }
