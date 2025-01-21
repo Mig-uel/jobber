51cf9e0a3fbcbb3394b482da10488d855c47d8fd
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-21 11:34:41 -0500

    feat: Implement JobsList component with job fetching and search functionality

--- components/JobsList.tsx
+++ components/JobsList.tsx
@@ -1,3 +1,42 @@
+'use client'
+
+import { getAllJobs } from '@/utils/actions'
+import { useQuery } from '@tanstack/react-query'
+import { useSearchParams } from 'next/navigation'
+import JobCard from './JobCard'
+
 export default function JobsList() {
-  return <div>JobsList</div>
+  const searchParams = useSearchParams()
+
+  const search = searchParams.get('search') || ''
+  const jobStatus = searchParams.get('jobStatus') || 'all'
+  const pageNumber = Number(searchParams.get('pageNumber')) || 1
+
+  const { data, isPending } = useQuery({
+    queryKey: ['jobs', search, jobStatus, pageNumber],
+
+    queryFn: () =>
+      getAllJobs({
+        jobStatus,
+        page: pageNumber,
+        search,
+      }),
+  })
+
+  const jobs = data?.jobs || []
+
+  if (isPending) return <h2 className='text-xl'>Loading jobs...</h2>
+
+  if (!jobs.length) return <h2 className='text-xl'>No jobs found :(</h2>
+
+  return (
+    <>
+      {/* button container */}
+      <div className='grid md:grid-cols-2 gap-8'>
+        {jobs.map((job) => (
+          <JobCard key={job.id} job={job} />
+        ))}
+      </div>
+    </>
+  )
 }
