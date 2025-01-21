a799f2db2524bea86b7fe257b3d9c2fdda3a2680
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-21 16:50:17 -0500

    feat: Implement data fetching and hydration for StatsContainer and ChartsContainer

--- app/(dashboard)/stats/page.tsx
+++ app/(dashboard)/stats/page.tsx
@@ -1,3 +1,33 @@
-export default function Page() {
-  return <div>Page</div>
+import ChartsContainer from '@/components/ChartsContainer'
+import StatsContainer from '@/components/StatsContainer'
+import { getChartData, getStats } from '@/utils/actions'
+
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query'
+
+export default async function Page() {
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery({
+    queryKey: ['stats'],
+
+    queryFn: () => getStats,
+  })
+
+  await queryClient.prefetchQuery({
+    queryKey: ['charts'],
+
+    queryFn: () => getChartData(),
+  })
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <StatsContainer />
+
+      <ChartsContainer />
+    </HydrationBoundary>
+  )
 }
