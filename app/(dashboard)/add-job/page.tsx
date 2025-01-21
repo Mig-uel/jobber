5e1787f7c3f11beae636d752620efdfcd0fd3533
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-21 08:57:12 -0500

    feat: Wrap AddJobForm in HydrationBoundary for React Query state management

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,9 +1,16 @@
 import { AddJobForm } from '@/components/index'
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query'
 
 export default function Page() {
+  const queryClient = new QueryClient()
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <AddJobForm />
-    </>
+    </HydrationBoundary>
   )
 }
