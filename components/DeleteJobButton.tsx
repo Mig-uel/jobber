c3544f533d188f951fc727f91c37c4ffd7ff6699
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-21 12:05:12 -0500

    feat: Implement DeleteJobButton with job deletion functionality and toast notifications

--- components/DeleteJobButton.tsx
+++ components/DeleteJobButton.tsx
@@ -1,5 +1,49 @@
+import { useToast } from '@/hooks/use-toast'
+import { deleteJob } from '@/utils/actions'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { Button } from './ui/button'
 
-export default function DeleteJobButton() {
-  return <Button variant='destructive'>Delete</Button>
+export default function DeleteJobButton({ id }: { id: string }) {
+  const { toast } = useToast()
+  const queryClient = useQueryClient()
+
+  const { isPending, mutate } = useMutation({
+    mutationFn: (id: string) => deleteJob(id),
+
+    onSuccess(data) {
+      if (!data) {
+        toast({
+          description: 'There was an error deleting the job ⛔',
+          variant: 'destructive',
+        })
+        return
+      }
+
+      queryClient.invalidateQueries({
+        queryKey: ['jobs'],
+      })
+      queryClient.invalidateQueries({
+        queryKey: ['stats'],
+      })
+      queryClient.invalidateQueries({
+        queryKey: ['charts'],
+      })
+
+      toast({ description: 'Job successfully deleted 🎉' })
+    },
+  })
+
+  const handleClick = () => mutate(id)
+
+  return (
+    <Button
+      type='submit'
+      variant='destructive'
+      disabled={isPending}
+      onClick={handleClick}
+      size='sm'
+    >
+      Delete
+    </Button>
+  )
 }
