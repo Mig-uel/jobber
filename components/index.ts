8bf0bc776259484e4f3ee3cc43da91d72f90ce43
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-20 19:35:41 -0500

    feat: Export AddJobForm component for job submission

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
+export { default as AddJobForm } from './AddJobForm'
 export { default as LinksDropdown } from './LinksDropdown'
 export { default as Navbar } from './Navbar'
 export { default as Sidebar } from './Sidebar'
