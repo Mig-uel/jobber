94c0d9115e54e223f1078b67b5c6533be5ff3606
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-21 10:13:45 -0500

    feat: Add getAllJobs action for fetching jobs with filtering options

--- utils/actions.ts
+++ utils/actions.ts
@@ -3,8 +3,15 @@
 import prisma from './db'
 import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
-import { createAndEditJobSchema, CreateAndEditJobType, JobType } from './types'
+import {
+  createAndEditJobSchema,
+  CreateAndEditJobType,
+  JobStatus,
+  JobType,
+} from './types'
+import { Prisma } from '@prisma/client'
 
+/** Authenticate or Redirect */
 function authenticateOrRedirect(): string {
   const { userId } = auth()
 
@@ -13,6 +20,7 @@ function authenticateOrRedirect(): string {
   return userId
 }
 
+/** Add Job Action  */
 export async function addJob(
   values: CreateAndEditJobType
 ): Promise<JobType | null> {
@@ -33,5 +41,84 @@ export async function addJob(
     if (error instanceof Error) console.log(error.message)
 
     return null
+  }
+}
+
+type GetAllJobsActionProps = {
+  search?: string
+  jobStatus?: string
+  page?: number
+  limit?: number
+}
+
+/** Get All Jobs */
+export async function getAllJobs({
+  jobStatus = 'all',
+  limit = 10,
+  page = 1,
+  search = '',
+}: GetAllJobsActionProps): Promise<{
+  jobs: JobType[]
+  count: number
+  page: number
+  totalPages: number
+}> {
+  const clerkId = authenticateOrRedirect()
+
+  try {
+    // prisma where clause
+    let where: Prisma.JobWhereInput = {
+      clerkId,
+    }
+
+    // if search term is provided
+    if (search)
+      where = {
+        ...where,
+        OR: [
+          {
+            position: {
+              contains: search,
+            },
+          },
+          {
+            company: {
+              contains: search,
+            },
+          },
+        ],
+      }
+
+    // if jobStatus is provided and is not the default 'all' value
+    if (jobStatus && jobStatus !== 'all')
+      where = {
+        ...where,
+
+        status: jobStatus,
+      }
+
+    const jobs: JobType[] = await prisma.job.findMany({
+      where,
+
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    return {
+      jobs,
+      count: 0,
+      page: 1,
+      totalPages: 0,
+    }
+  } catch (error) {
+    if (error instanceof Error) console.log(error.message)
+
+    return {
+      jobs: [],
+      count: 0,
+      page: 1,
+      totalPages: 0,
+    }
   }
 }
