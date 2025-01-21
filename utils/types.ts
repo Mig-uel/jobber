0fa927cfbfd89535756b72a7dd0fc25a21d01d27
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-21 07:37:42 -0500

    feat: Add job creation and editing schema with validation

--- utils/types.ts
+++ utils/types.ts
@@ -23,3 +23,23 @@ export enum JobMode {
   PartTime = 'part-time',
   Internship = 'internship',
 }
+
+export const createAndEditJobSchema = zod.object({
+  position: zod.string().min(2, {
+    message: 'Position must be at least 2 characters.',
+  }),
+
+  company: zod.string().min(2, {
+    message: 'Company must be at least 2 characters.',
+  }),
+
+  location: zod.string().min(2, {
+    message: 'Location must be at least 2 characters.',
+  }),
+
+  status: zod.nativeEnum(JobStatus),
+
+  mode: zod.nativeEnum(JobMode),
+})
+
+export type CreateAndEditJobType = zod.infer<typeof createAndEditJobSchema>
