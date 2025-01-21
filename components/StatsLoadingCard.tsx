b22b3b464ed2387fb131eec3abc6f3519ddf0180
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-21 17:45:05 -0500

    feat: Add StatsLoadingCard component for displaying loading state in statistics

---
+++ components/StatsLoadingCard.tsx
@@ -0,0 +1,19 @@
+import { Card, CardHeader } from './ui/card'
+import { Skeleton } from './ui/skeleton'
+
+export default function StatsLoadingCard() {
+  return (
+    <Card className='w-[330px] h-[88px]'>
+      <CardHeader className='flex row justify-between items-center'>
+        <div className='flex items-center space-x-4'>
+          <Skeleton className='h-12 w-12 rounded-full' />
+
+          <div className='space-y-12'>
+            <Skeleton className='h-4 w-[150px]' />
+            <Skeleton className='h-4 w-[100px]' />
+          </div>
+        </div>
+      </CardHeader>
+    </Card>
+  )
+}
