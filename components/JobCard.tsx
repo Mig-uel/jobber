2c7e9c5c7110df1163ee5a8c8b4855f50fbd4a06
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-21 11:44:54 -0500

    feat: Enhance JobCard component with layout and functionality for job details and actions

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,5 +1,35 @@
 import type { JobType } from '@/utils/types'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card'
+import { Separator } from './ui/separator'
+import { Button } from './ui/button'
+import Link from 'next/link'
+import DeleteJobButton from './DeleteJobButton'
 
 export default function JobCard({ job }: { job: JobType }) {
-  return <div>JobCard</div>
+  return (
+    <Card className='bg-muted'>
+      <CardHeader>
+        <CardTitle>{job.position}</CardTitle>
+        <CardDescription>{job.company}</CardDescription>
+      </CardHeader>
+
+      <Separator />
+
+      <CardContent></CardContent>
+
+      <CardFooter className='flex gap-4'>
+        <Button asChild>
+          <Link href={`/job/${job.id}`}>Edit</Link>
+        </Button>
+        <DeleteJobButton />
+      </CardFooter>
+    </Card>
+  )
 }
